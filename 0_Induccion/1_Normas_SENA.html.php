1880bdabeb177148fdecf8d3330f3cbfd1c349b5
Diego Alonso Ojeda Medina <daom89@gmail.com>
2021-09-08 10:27:40 -0500

    Anuncion Normas

---
+++ 0_Induccion/1_Normas_SENA.html.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Normas de Convivencia - Inducción</title>
+    <link href="../anuncios/bootstrap-5.1.0-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../anuncios/css/maven-font.css" rel="stylesheet" type="text/css">
+    <link href="../anuncios/css/main.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
+        body {
+            width: 800px;
+            background: url(../anuncios/img/Fondo03_LargeNaranja.png) no-repeat;
+            background-size: 800px;
+        }
+    </style>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row mainTitle">
+        <div class="text-center align-content-center">
+            Tgo en Analisis y Desarrollo de Sistemas de Informacion - 2338894
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 text-center">
+            <div class="pt-2 pb-1">
+                <strong class="textoVerde tituloMedium">Centro Minero </strong> - <strong
+                        class="textoNaranja tituloMedium">Reglamento</strong>
+            </div>
+        </div>
+        <div class="row contentAnuncio">
+            <div class="col-12 text-center">
+                <iframe title="REGLAMENTO DEL APRENDIZ SENA.pptx"
+                        src="https://drive.google.com/file/d/10IqNsUMcj7eUPwoqDEYZC0BlEKTdBDeh/preview"
+                        width="600" height="550" allow="autoplay"></iframe>
+            </div>
+        </div>
+    </div>
+    <div class="row-fluid">
+        <div class="pt-4 text-center textoBlanco textFooter">
+            <span>Servicio Nacional de Aprendizaje, SENA - Regional Boyac&aacute; - Sogamoso - Diego Ojeda</span>
+        </div>
+    </div>
+</div>
+<script src="../anuncios/bootstrap-5.1.0-dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
